796e7814ddcc9b0dd8ddb7341a89347a7ed49cec
Alan Pason <alan.pason132@gmail.com>
2022-12-28 14:27:22 +0100

    feat: display result

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 const digitsButtons = document.querySelectorAll(".digits");
 const operatorButtons = document.querySelectorAll(".operators");
+const operationsInput = document.querySelector(".operations-input");
+const result = document.querySelector(".result");
 let firstNumber = "";
 let secondNumber = "";
 let total = 0;
 let mathOperator = "";
 let isFirstOperation = true;
 
-function displayResult() {}
+function displayResult() {
+  if (isFirstOperation) return (result.textContent = firstNumber);
+
+  operationsInput.textContent = `${total} ${mathOperator}`;
+  result.textContent = secondNumber;
+}
 
 function enterNumbers() {
   isFirstOperation ? (firstNumber += this.value) : (secondNumber += this.value);
-  console.log(total);
+  displayResult();
 }
 
 function operate() {
   if (isFirstOperation) {
     total = firstNumber;
     mathOperator = this.value;
-    return (isFirstOperation = false);
+    isFirstOperation = false;
+    return displayResult();
   }
 
-  if (secondNumber == "") return (mathOperator = this.value);
+  if (secondNumber == "") {
+    mathOperator = this.value;
+    return displayResult();
+  }
 
   parseSecond = parseFloat(secondNumber);
   parseTotal = parseFloat(total);
@@ -33,6 +44,7 @@ function operate() {
 
   secondNumber = "";
   mathOperator = this.value;
+  displayResult();
 }
 
 digitsButtons.forEach((digitBtn) => digitBtn.addEventListener("click", enterNumbers));
