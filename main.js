62758882d68261ce7a3279fe1daea3ae1f84ca01
Alan Pason <alan.pason132@gmail.com>
2022-12-28 14:16:39 +0100

    feat: save numbers and does mathematical operations

--- main.js
+++ main.js
@@ -3,15 +3,34 @@ let operatorButtons = document.querySelectorAll(".operators");
 let firstNumber = "";
 let secondNumber = "";
 let total = 0;
+let mathOperator = "";
 let isFirstOperation = true;
 
 function enterNumbers() {
   isFirstOperation ? (firstNumber += this.value) : (secondNumber += this.value);
-  console.log(firstNumber);
+  console.log(total);
 }
 
 function operate() {
-  isFirstOperation = false;
+  if (isFirstOperation) {
+    total = firstNumber;
+    mathOperator = this.value;
+    return (isFirstOperation = false);
+  }
+
+  if (secondNumber == "") return (mathOperator = this.value);
+
+  parseSecond = parseFloat(secondNumber);
+  parseTotal = parseFloat(total);
+
+  if (mathOperator == "+") total = parseTotal + parseSecond;
+  if (mathOperator == "-") total = parseTotal - parseSecond;
+  if (mathOperator == "x") total = parseTotal * parseSecond;
+  if (mathOperator == "/") total = parseTotal / parseSecond;
+  if (mathOperator == "%") total = parseTotal % parseSecond;
+
+  secondNumber = "";
+  mathOperator = this.value;
 }
 
 digitsButtons.forEach((digitBtn) => digitBtn.addEventListener("click", enterNumbers));
